94b2da3fb44c9bd5f446977ddb06f80c02402197
hyjang <hyjang@10.139.4.159>
2016-06-15 23:23:13 +0900

    Initial Commit with Mariadb Docker Builder

---
+++ docker/db/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /var/lib/mysql
+mkdir -p /var/lib/mysql
+chmod 777 /var/lib/mysql
+
+docker run -d --name os_db --net host -v /var/lib/mysql:/var/lib/mysql:rw ciscokr/openstack_mariadb
