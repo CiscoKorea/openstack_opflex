2c1492e63f38c4e33810b62d7b06cb52283fa1e0
hyjang <hyjang@10.139.4.159>
2016-06-18 00:24:41 +0900

    new

--- docker/openstack/ciscokr/bin/setting.sh
+++ docker/openstack/ciscokr/bin/setting.sh
@@ -9,9 +9,14 @@ $CB/do_permissions.sh /var/log/mariadb/
 #$CB/do_permissions.sh /var/run/
 
 sed -i "s/PASSWORD/$PASSWORD/g" 											/etc/my.cnf.d/client.cnf
+
 sed -i "s/#ServerName www.example.com:80/ServerName $HOSTNAME/g" 			/etc/httpd/conf/httpd.conf
 sed -i "s/Listen 80/Listen 0.0.0.0:80/g" 									/etc/httpd/conf/httpd.conf
+
+sed -i "s/ADMIN_TOKEN/$PASSWORD/"											/etc/keystone/keystone.conf
 sed -i "s/PASSWORD/$PASSWORD/" 												/etc/keystone/keystone.conf
 sed -i "s/HOSTNAME/$HOSTNAME/" 												/etc/keystone/keystone.conf
-sed -i "s/OPENSTACK_HOST = \"HOSTNAME\"/OPENSTACK_HOST = \"$HOSTNAME\"/g" 	/etc/openstack-dashboard/local_settings
+
+sed -i "s/OPENSTACK_HOST = \"HOSTIP\"/OPENSTACK_HOST = \"$HOSTIP\"/g" 		/etc/openstack-dashboard/local_settings
+
 #sed -i "s/WSGISocketPrefix run\/wsgi/WSGISocketPrefix \/tmp\/wsgi/g" 		/etc/httpd/conf.d/openstack-dashboard.conf
