acf730fc0c4fd761263ab17647b34d79c70a3aad
hyjang <hyjang@10.139.4.159>
2016-06-18 19:46:25 +0900

    commit glance

--- ciscokr/bin/runlevel_5.sh
+++ ciscokr/bin/runlevel_5.sh
@@ -5,3 +5,12 @@ echo "START RUN LEVEL 5"
 CB=/root/ciscokr/bin
 CF=/root/ciscokr/files
 
+sudo -u glance -E -s /usr/bin/glance-api &
+sudo -u glance -E -s /usr/bin/glance-registry &
+
+#exec /usr/bin/nova-api &
+#exec /usr/bin/nova-cert &
+#exec /usr/bin/nova-consoleauth &
+#exec /usr/bin/nova-scheduler &
+#exec /usr/bin/nova-conductor &
+#exec /usr/bin/nova-novncproxy --web /usr/share/novnc/ $OPTIONS &
