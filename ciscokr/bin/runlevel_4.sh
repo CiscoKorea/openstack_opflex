431e4d56d106415e767c78d126bfa4d34490b59c
hyjang <hyjang@10.139.4.159>
2016-06-18 18:58:10 +0900

    commit

--- ciscokr/bin/runlevel_4.sh
+++ ciscokr/bin/runlevel_4.sh
@@ -27,15 +27,14 @@ if [ ! -f /.registered ]; then
 	openstack user create --domain default --password $PASSWORD neutron
 	
 	# Register Services
-
-	export OS_PROJECT_DOMAIN_ID=default
-	export OS_USER_DOMAIN_ID=default
-	export OS_PROJECT_NAME=admin
-	export OS_TENANT_NAME=admin
-	export OS_USERNAME=admin
-	export OS_PASSWORD=$PASSWORD
-	export OS_AUTH_URL=http://$HOSTIP:35357/v3
-	export OS_IDENTITY_API_VERSION=3
+#	export OS_PROJECT_DOMAIN_ID=default
+#	export OS_USER_DOMAIN_ID=default
+#	export OS_PROJECT_NAME=admin
+#	export OS_TENANT_NAME=admin
+#	export OS_USERNAME=admin
+#	export OS_PASSWORD=$PASSWORD
+#	export OS_AUTH_URL=http://$HOSTIP:35357/v3
+#	export OS_IDENTITY_API_VERSION=3
 	
 	# Register Roles
 	openstack role add --project service --user glance admin
