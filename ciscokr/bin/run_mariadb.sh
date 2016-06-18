634dd2660063348fa4cd6f40244a801f35830baf
hyjang <hyjang@10.139.4.159>
2016-06-18 16:08:39 +0900

    update

--- ciscokr/bin/run_mariadb.sh
+++ ciscokr/bin/run_mariadb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #su -s /bin/sh -c "/var/lib/mysql/run_mariadb_sudo.sh mysqld_safe" mysql &
-sudo -u mysql -E /root/openstack_opflex/bin/run_mariadb_sudo.sh mysqld_safe &
+sudo -u mysql -E /root/openstack_opflex/ciscokr/bin/run_mariadb_sudo.sh mysqld_safe &
