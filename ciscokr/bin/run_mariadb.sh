02aeec6f9a7d80feeb84f81c157d6f8cdff5e527
hyjang <hyjang@10.139.4.159>
2016-06-18 16:04:25 +0900

    update

--- ciscokr/bin/run_mariadb.sh
+++ ciscokr/bin/run_mariadb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-su -E -s /bin/sh -c "/var/lib/mysql/run_mariadb_sudo.sh mysqld_safe" mysql &
+#su -s /bin/sh -c "/var/lib/mysql/run_mariadb_sudo.sh mysqld_safe" mysql &
+sudo -u mysql -E -s /run_mariadb_sudo.sh mysqld_safe
