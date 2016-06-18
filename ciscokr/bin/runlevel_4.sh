c93f780825ae57ea37b95a87c92d54212da31f5b
hyjang <hyjang@10.139.4.159>
2016-06-18 18:53:40 +0900

    commit

--- ciscokr/bin/runlevel_4.sh
+++ ciscokr/bin/runlevel_4.sh
@@ -20,6 +20,21 @@ if [ ! -f /.registered ]; then
 	openstack role create admin
 	openstack role add --project admin --user admin admin
 	openstack project create --domain default --description "Service Project" service
+	
+	# Create User
+	openstack user create --domain default --password $PASSWORD glance
+	openstack role add --project service --user glance admin
+	openstack service create --name glance --description "OpenStack Image service" image
+	
+	openstack user create --domain default --password $PASSWORD nova
+	openstack role add --project service --user nova admin
+	openstack service create --name nova --description "OpenStack Compute" compute
+	
+	openstack user create --domain default --password $PASSWORD neutron
+	openstack role add --project service --user neutron admin
+	openstack service create --name neutron --description "OpenStack Networking" network
+	
+	# Register Services
 
 	export OS_PROJECT_DOMAIN_ID=default
 	export OS_USER_DOMAIN_ID=default
@@ -37,19 +52,6 @@ if [ ! -f /.registered ]; then
 	su -s /bin/sh -c "nova-manage db sync" nova
 	mysql -u root -p -e "CREATE DATABASE neutron; GRANT ALL PRIVILEGES ON neutrons.* TO 'neutron'@'localhost' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'%' IDENTIFIED BY '$PASSWORD';"
 	su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
-
-	# Create User
-	openstack user create --domain default --password $PASSWORD glance
-	openstack role add --project service --user glance admin
-	openstack service create --name glance --description "OpenStack Image service" image
-	
-	openstack user create --domain default --password $PASSWORD nova
-	openstack role add --project service --user nova admin
-	openstack service create --name nova --description "OpenStack Compute" compute
-	
-	openstack user create --domain default --password $PASSWORD neutron
-	openstack role add --project service --user neutron admin
-	openstack service create --name neutron --description "OpenStack Networking" network
 	
 	# Create Endpoint
 	openstack endpoint create --region RegionOne image public http://$HOSTIP:9292
