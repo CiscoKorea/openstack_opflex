8949bbf85e156673ccc640abc59cdb6bfaa4607b
hyjang <hyjang@10.139.4.159>
2016-06-19 20:55:00 +0900

    commit

--- ciscokr/bin/run_glance.sh
+++ ciscokr/bin/run_glance.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo -u glance -E -s /usr/bin/glance-api &
-sudo -u glance -E -s /usr/bin/glance-registry &
-
+sudo -u glance -E -s /usr/bin/glance-api
+sudo -u glance -E -s /usr/bin/glance-registry
+sleep 1
+glance image-create --name "cirros-0.3.4-x86_64" --file /root/ciscokr/image/cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare --visibility public --progress
