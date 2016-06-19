3b40840c18ac931e64dddd041c6c36e87468b1a8
hyjang <hyjang@10.139.4.159>
2016-06-19 21:36:29 +0900

    commit

--- ciscokr/bin/run_glance.sh
+++ ciscokr/bin/run_glance.sh
@@ -3,4 +3,14 @@
 sudo -u glance -E -s /usr/bin/glance-api &
 sudo -u glance -E -s /usr/bin/glance-registry &
 sleep 2
+
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=$PASSWORD
+export OS_AUTH_URL=http://$HOSTIP:35357/v3
+export OS_IMAGE_API_VERSION=2
+
 glance image-create --name "cirros-0.3.4-x86_64" --file /root/ciscokr/image/cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare --visibility public --progress &
