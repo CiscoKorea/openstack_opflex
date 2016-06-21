e0e2517d062370e73511c843550d33bee9e2897b
hyjang <hyjang@10.139.4.214>
2016-06-21 16:49:46 +0900

    commit

--- controller/ciscokr/setting.sh
+++ controller/ciscokr/setting.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "Setting!!!
+
 # Mysql
+echo "Mysql"
 $_BIN/do_permissions.sh /var/lib/mysql/
 $_BIN/do_permissions.sh /var/log/mariadb/
 cp $_BIN/run_mariadb_sudo.sh /var/lib/mysql/
@@ -8,35 +11,42 @@ chown mysql /var/lib/mysql/run_mariadb_sudo.sh
 sed -i "s/PASSWORD/$PASSWORD/g" 											/etc/my.cnf.d/client.cnf
 
 # Rabbit MQ
+echo "RabbitMQ"
 cat >/etc/rabbitmq/rabbitmq.config <<EOF
 [ {rabbit, [{default_user, <<"admin">>}, {default_pass, <<"$PASSWORD">>}]} ].
 EOF
 /usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_management >> /root/rabbit.log
 
 # HTTPD
+echo "HTTP"
 sed -i "s/#ServerName www.example.com:80/ServerName $HOSTNAME/g" 			/etc/httpd/conf/httpd.conf
 sed -i "s/Listen 80/Listen 0.0.0.0:80/g" 									/etc/httpd/conf/httpd.conf
 
 # Keystone
+echo "Keystone"
 sed -i "s/ADMIN_TOKEN/$PASSWORD/"											/etc/keystone/keystone.conf
 sed -i "s/PASSWORD/$PASSWORD/" 												/etc/keystone/keystone.conf
 sed -i "s/HOSTNAME/$HOSTNAME/" 												/etc/keystone/keystone.conf
 
 # Horizon
+echo "Horizon"
 sed -i "s/OPENSTACK_HOST = \"HOSTIP\"/OPENSTACK_HOST = \"$HOSTIP\"/g" 		/etc/openstack-dashboard/local_settings
 
 # Glance
+echo "Glance"
 sed -i "s/PASSWORD/$PASSWORD/g" 											/etc/glance/glance-api.conf
 sed -i "s/HOSTIP/$HOSTIP/g" 												/etc/glance/glance-api.conf
 sed -i "s/PASSWORD/$PASSWORD/g" 											/etc/glance/glance-registry.conf
 sed -i "s/HOSTIP/$HOSTIP/g" 												/etc/glance/glance-registry.conf
 
 # Nova
+echo "Nova"
 sed -i "s/HOSTNAME/$HOSTNAME/" 												/etc/nova/nova.conf
 sed -i "s/PASSWORD/$PASSWORD/g" 											/etc/nova/nova.conf
 sed -i "s/HOSTIP/$HOSTIP/g" 												/etc/nova/nova.conf
 
 # Neutron
+echo "Neutron"
 sed -i "s/HOSTNAME/$HOSTNAME/" 												/etc/neutron/neutron.conf
 sed -i "s/PASSWORD/$PASSWORD/g" 											/etc/neutron/neutron.conf
 sed -i "s/HOSTIP/$HOSTIP/g" 												/etc/neutron/neutron.conf
@@ -46,6 +56,7 @@ sed -i "s/HOSTIP/$HOSTIP/g" 												/etc/neutron/plugins/ml2/ml2_conf.ini
 ln -s /etc/neutron/plugins/ml2/ml2_conf.ini /etc/neutron/plugin.ini
 
 # Opflex
+echo "OpFlex"
 sed -i "s/APICID/$APICID/g" >> /etc/neutron/plugins/ml2/ml2_conf_cisco_apic.ini
 sed -i "s/APICHOST/$APICHOST/g" >> /etc/neutron/plugins/ml2/ml2_conf_cisco_apic.ini
 sed -i "s/APICUSER/$APICUSER/g" >> /etc/neutron/plugins/ml2/ml2_conf_cisco_apic.ini
