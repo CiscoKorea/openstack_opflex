34f128dbaf91d2429a45b683ca3b87e09f989a1b
Hye-Churn, Jang <hyjang@cisco.com>
2016-06-27 19:46:27 +0900

    commit

--- controller/ciscokr/runlevel_2.sh
+++ controller/ciscokr/runlevel_2.sh
@@ -5,8 +5,8 @@ echo "START RUN LEVEL 2"
 if [ ! -f /.rabbit_registered ]; then
 
 	# Create Rabbit User
-	rabbitmqctl add_user openstack $HOST_PASS
-	rabbitmqctl set_permissions openstack ".*" ".*" ".*"
+	rabbitmqctl add_user openstack $HOST_PASS >> /tmp/running.log
+	rabbitmqctl set_permissions openstack ".*" ".*" ".*" >> /tmp/running.log
 	
 	# Touch !
 	touch /.rabbit_registered
