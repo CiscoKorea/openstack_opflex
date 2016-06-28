f1d50ca9a6c561d7631115a2c39c4a661dbfe776
Hye-Churn Jang <hyjang@cisco.com>
2016-06-28 23:47:36 +0900

    commit

---
+++ controller/image/install_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PWD=`pwd`
+
+docker pull centos:7
+
+docker run \
+ -ti \
+ --privileged \
+ --net host \
+ --name base \
+ -v $PWD:/root/image:ro \
+ centos:7 \
+ /root/image/installer.sh
+
+docker commit base ciscokr/openstack_base
+docker rm -f base
+
+docker build --rm --tag ciscokr/openstack_opflex .
