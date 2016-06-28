00871d70602f67fef46d58d0f6b59ee20849daaf
Hye-Churn, Jang <hyjang@cisco.com>
2016-06-29 00:11:41 +0900

    commit

--- controller/image/installer.sh
+++ controller/image/installer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 yum install -y --setopt=tsflags=nodocs epel-release
-yum install -y --setopt=tsflags=nodocs https://repos.fedorapeople.org/repos/openstack/openstack-liberty/rdo-release-liberty-3.noarch.rpm
+#yum install -y --setopt=tsflags=nodocs https://repos.fedorapeople.org/repos/openstack/openstack-liberty/rdo-release-liberty-3.noarch.rpm
+yum install -y --setopt=tsflags=nodocs https://repos.fedorapeople.org/repos/openstack/openstack-liberty/rdo-release-liberty-5.noarch.rpm
 yum install -y --setopt=tsflags=nodocs openstack-selinux
 
 yum update -y && yum upgrade -y
